09b49c28873b52b5d465591bd7256db68fc23cd1
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2015-12-24 16:20:32 +0300

    Initial Commit

---
+++ CoreDataStack/CoreDataStack.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataStack.h
+//  CoreDataStack
+//
+//  Created by Aliaksandr Skulin on 12/24/15.
+//  Copyright © 2015 Aliaksandr Skulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataStack.
+FOUNDATION_EXPORT double CoreDataStackVersionNumber;
+
+//! Project version string for CoreDataStack.
+FOUNDATION_EXPORT const unsigned char CoreDataStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataStack/PublicHeader.h>
+
+
