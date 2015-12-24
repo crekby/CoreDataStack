e526c9f19ddd0ebb7321fbe0f669a4ea30371aab
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2015-12-24 17:05:22 +0300

    added database manager

---
+++ CoreDataStack/Classes/ITDatabaseManager.h
@@ -0,0 +1,13 @@
+//
+//  ITDatabaseManager.h
+//  CoreDataStack
+//
+//  Created by Aliaksandr Skulin on 12/24/15.
+//  Copyright © 2015 Aliaksandr Skulin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ITDatabaseManager : NSObject
+
+@end
---
+++ CoreDataStack/Classes/ITDatabaseManager.m
@@ -0,0 +1,13 @@
+//
+//  ITDatabaseManager.m
+//  CoreDataStack
+//
+//  Created by Aliaksandr Skulin on 12/24/15.
+//  Copyright © 2015 Aliaksandr Skulin. All rights reserved.
+//
+
+#import "ITDatabaseManager.h"
+
+@implementation ITDatabaseManager
+
+@end
