64420eef3b5b8079c43e865759d4c71672d4bfa2
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2015-12-24 18:35:30 +0300

    added persistence initialisation

--- Classes/ITDatabaseManager.m
+++ Classes/ITDatabaseManager.m
@@ -7,16 +7,20 @@
 //
 
 #import "ITDatabaseManager.h"
+#import <CoreData/CoreData.h>
 
 @interface ITDatabaseManager()
 
 @property (nonatomic, strong) NSManagedObjectModel *model;
+@property (nonatomic, strong) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 @property (nonatomic, strong) NSString *storeType;
 @property (nonatomic, strong) NSURL *storeURL;
 
 @end
 
 @implementation ITDatabaseManager
+
+#pragma mark - Inits
 
 - (instancetype)initWithModel:(NSManagedObjectModel *)model storeName:(NSString *)storeName storeType:(NSString *)storeType
 {
@@ -29,6 +33,8 @@
         _model = model;
         _storeType = storeType;
         _storeURL = [[ITDatabaseManager applicationDocumentsDirectory] URLByAppendingPathComponent:storeName];
+        NSAssert([self initialisePersistenceStore], @"Unable to initialise persistence store");
+        
     }
     return self;
 }
@@ -38,9 +44,79 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+#pragma mark - Class Methods
+
 + (NSURL *)applicationDocumentsDirectory
 {
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
+#pragma mark - Helpers
+
+- (BOOL)persistentStoreExistsAtURL:(NSURL *)url
+{
+    NSError *error;
+    BOOL resourceIsReachable = [url checkResourceIsReachableAndReturnError:&error];
+    return resourceIsReachable;
+}
+
+- (BOOL)isModel:(NSManagedObjectModel *)model compatibleWithPersistentStoreAtURL:(NSURL *)url
+{
+    // can't be compatible with something that doesn't exist :D
+    if (![self persistentStoreExistsAtURL:url]) {
+        return NO;
+    }
+    
+    NSError *error;
+    NSDictionary *metaData = [NSPersistentStoreCoordinator metadataForPersistentStoreOfType:self.storeType
+                                                                                        URL:url
+                                                                                      error:&error];
+    
+    // If there was an error retrieving the metadata, not compatible
+    if (error) {
+        return NO;
+    }
+    
+    // If there was no metadata, not compatible
+    if (metaData == nil) {
+        return NO;
+    }
+    
+    BOOL compatible = [model isConfiguration:nil compatibleWithStoreMetadata:metaData];
+    return compatible;
+}
+
+#pragma mark - Private Methods
+
+- (BOOL)initialisePersistenceStore
+{
+    self.persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.model];
+    if (!self.persistentStoreCoordinator) {
+        return NO;
+    }
+
+    BOOL exists = [self persistentStoreExistsAtURL:self.storeURL];
+    if (exists) {
+        BOOL compatible = [self isModel:self.model compatibleWithPersistentStoreAtURL:self.storeURL];
+        if (!compatible) {
+            return NO;
+        }
+    }
+    
+    NSDictionary *storeOptions = @{NSMigratePersistentStoresAutomaticallyOption:@YES,
+                                   NSInferMappingModelAutomaticallyOption:@YES};
+    
+    NSError *error;
+    [self.persistentStoreCoordinator addPersistentStoreWithType:self.storeType
+                                                  configuration:nil
+                                                            URL:self.storeURL
+                                                        options:storeOptions
+                                                          error:&error];
+    
+    if (error) {
+        return NO;
+    }
+    return YES;
 }
 
 @end
