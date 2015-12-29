f72c61b7d7c130bf26c587eddd35b1a85b980155
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2015-12-29 12:29:40 +0300

    added functions descriptions

--- Classes/ITDatabaseManager.h
+++ Classes/ITDatabaseManager.h
@@ -16,12 +16,33 @@
 @property (nonatomic, strong, readonly) ITManagedObjectContext *mainManagedObjectContext;
 @property (nonatomic, strong, readonly) ITManagedObjectContext *backgroundManagedObjectContext;
 
+/**
+ URL to Documents directory in application sandbox.
+ */
 + (NSURL *)applicationDocumentsDirectory;
 
 - (instancetype)init NS_UNAVAILABLE;
+
+/**
+ Returns Database manager instance with given parametrs.
+ @param model Model object.
+ @param storeName Name for store on file system.
+ @param storeType Store type (NSSQLiteStoreType, NSBinaryStoreType, NSInMemoryStoreType).
+ */
 - (instancetype)initWithModel:(NSManagedObjectModel *)model storeName:(NSString *)storeName storeType:(NSString *)storeType NS_DESIGNATED_INITIALIZER;
 
+/**
+ Executes given block in main trhead. 
+ @warning DO NOT USE MAIN THREAD CONTEXT FOR CHANGES, USE IT ONLY FOR FETCHING.
+ @param backgroundOperation Block for execution.
+ */
 - (void)executeMainThreadOperation:(void (^)(NSManagedObjectContext *))mainThreadOperation;
+
+/**
+ Executes given block in background.
+ @warning USE BACKGROUND CONTEXT FOR MAKING CHANGES IN YOUR MODEL, NOT MAIN CONTEXT.
+ @param backgroundOperation Block for execution.
+ */
 - (void)executeBackgroundOperation:(void (^)(NSManagedObjectContext *))backgroundOperation;
 
 @end
