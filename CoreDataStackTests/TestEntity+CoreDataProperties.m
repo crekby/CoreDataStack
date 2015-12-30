8b8276c387bf6ac5c349a0e5a4f6174d015833c7
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2015-12-30 13:47:02 +0300

    added test model

---
+++ CoreDataStackTests/TestEntity+CoreDataProperties.m
@@ -0,0 +1,18 @@
+//
+//  TestEntity+CoreDataProperties.m
+//  CoreDataStack
+//
+//  Created by Aliaksandr Skulin on 12/30/15.
+//  Copyright © 2015 Aliaksandr Skulin. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "TestEntity+CoreDataProperties.h"
+
+@implementation TestEntity (CoreDataProperties)
+
+@dynamic testProperty;
+
+@end
