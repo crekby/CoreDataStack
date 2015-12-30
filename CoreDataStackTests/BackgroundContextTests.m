fdaea22d760cc0d71b3811f8dae1bd35b9834ea6
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2015-12-30 15:10:13 +0300

    added test for background context

---
+++ CoreDataStackTests/BackgroundContextTests.m
@@ -0,0 +1,37 @@
+//
+//  BackgroundContextTests.m
+//  CoreDataStack
+//
+//  Created by Aliaksandr Skulin on 12/30/15.
+//  Copyright © 2015 Aliaksandr Skulin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "TestEntity.h"
+#import "CoreDataStackTestCase.h"
+
+static NSString *kITTestEntityName = @"TestEntity";
+
+@interface BackgroundContextTests : CoreDataStackTestCase
+
+@end
+
+@implementation BackgroundContextTests
+
+- (void)testThatBackroundContextReturnObjectInMainContext
+{
+    XCTestExpectation* expectation = [self expectationWithDescription:@"Wait expectation"];
+    [self.databaseManager executeBackgroundOperation:^NSArray *(NSManagedObjectContext *context) {
+        TestEntity *object = [NSEntityDescription insertNewObjectForEntityForName:kITTestEntityName inManagedObjectContext:self.databaseManager.backgroundManagedObjectContext];
+        return @[object];
+    } mainThreadOperation:^(NSError *error, NSArray *result) {
+        XCTAssertNil(error);
+        TestEntity *object = result.firstObject;
+        XCTAssertNil(object);
+        XCTAssert(object.managedObjectContext.concurrencyType == NSMainQueueConcurrencyType, @"returned object is not in main context");
+        [expectation fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:5 handler:nil];
+}
+
+@end
