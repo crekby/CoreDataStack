5f73a8e6397277d9075e3cce903b867354c9760a
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2015-12-30 13:47:28 +0300

    added exceptions test for main context

--- CoreDataStackTests/CoreDataStackTests.m
+++ CoreDataStackTests/CoreDataStackTests.m
@@ -7,8 +7,13 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <CoreData/CoreData.h>
+#import "ITDatabaseManager.h"
+#import "TestEntity.h"
 
 @interface CoreDataStackTests : XCTestCase
+
+@property (nonatomic, strong) ITDatabaseManager *databaseManager;
 
 @end
 
@@ -16,7 +21,9 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSString *storeName = NSStringFromSelector([self.invocation selector]);
+    NSURL *modelURL = [[NSBundle bundleForClass:self.class] URLForResource:@"TestModel" withExtension:@"momd"];
+    self.databaseManager = [[ITDatabaseManager alloc] initWithModel:[[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL] storeName:storeName storeType:NSInMemoryStoreType];
 }
 
 - (void)tearDown {
@@ -24,16 +31,37 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testThatInsertObjectInMainContextWillTrowExeption
+{
+    XCTAssertThrowsSpecific([NSEntityDescription insertNewObjectForEntityForName:@"TestEntity" inManagedObjectContext:self.databaseManager.mainManagedObjectContext], NSException, @"Changes in main context is not allowed. Please use background context.");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)testThatDeletingObjectFromMainContextWillTrowExeption
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Wait expectation"];
+    [self.databaseManager executeBackgroundOperation:^NSArray *(NSManagedObjectContext *context) {
+        TestEntity *object = [NSEntityDescription insertNewObjectForEntityForName:@"TestEntity" inManagedObjectContext:self.databaseManager.backgroundManagedObjectContext];
+        return @[object];
+    } mainThreadOperation:^(NSError *error, NSArray *result) {
+        TestEntity *object = result.firstObject;
+        XCTAssertThrowsSpecific([self.databaseManager.mainManagedObjectContext deleteObject:object], NSException, @"Changes in main context is not allowed. Please use background context.");
+        [expectation fulfill];
     }];
+    [self waitForExpectationsWithTimeout:5 handler:nil];
+}
+
+- (void)testThatChangingObjectInMainContextWillTrowExeption
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Wait expectation"];
+    [self.databaseManager executeBackgroundOperation:^NSArray *(NSManagedObjectContext *context) {
+        TestEntity *object = [NSEntityDescription insertNewObjectForEntityForName:@"TestEntity" inManagedObjectContext:self.databaseManager.backgroundManagedObjectContext];
+        return @[object];
+    } mainThreadOperation:^(NSError *error, NSArray *result) {
+        TestEntity *object = result.firstObject;
+        XCTAssertThrowsSpecific([object setValue:@"1" forKey:@"testProperty"], NSException, @"Changes in main context is not allowed. Please use background context.");
+        [expectation fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:5 handler:nil];
 }
 
 @end
