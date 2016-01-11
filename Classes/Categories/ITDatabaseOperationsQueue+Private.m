1c85b9f094e505a6c02527b22aed1695b000a66a
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2016-01-11 18:05:37 +0300

    added private category setters and getters

--- Classes/Categories/ITDatabaseOperationsQueue+Private.m
+++ Classes/Categories/ITDatabaseOperationsQueue+Private.m
@@ -7,11 +7,42 @@
 //
 
 #import "ITDatabaseOperationsQueue+Private.h"
+#import <objc/runtime.h>
+
+static char kITModelKey;
+static char kITReadOnlyContextKey;
+static char kITChangesContextKey;
 
 @implementation ITDatabaseOperationsQueue(Private)
 
-@dynamic model;
-@dynamic readOnlyContext;
-@dynamic changesContext;
+- (void)setModel:(NSManagedObjectModel *)model
+{
+    objc_setAssociatedObject(self, &kITModelKey, model, OBJC_ASSOCIATION_RETAIN);
+}
+
+- (NSManagedObjectModel *)model
+{
+    return objc_getAssociatedObject(self, &kITModelKey);
+}
+
+- (void)setReadOnlyContext:(NSManagedObjectContext *)readOnlyContext
+{
+    objc_setAssociatedObject(self, &kITReadOnlyContextKey, readOnlyContext, OBJC_ASSOCIATION_RETAIN);
+}
+
+- (NSManagedObjectContext *)readOnlyContext
+{
+    return objc_getAssociatedObject(self, &kITReadOnlyContextKey);
+}
+
+- (void)setChangesContext:(NSManagedObjectContext *)changesContext
+{
+    objc_setAssociatedObject(self, &kITChangesContextKey, changesContext, OBJC_ASSOCIATION_RETAIN);
+}
+
+- (NSManagedObjectContext *)changesContext
+{
+    return objc_getAssociatedObject(self, &kITChangesContextKey);
+}
 
 @end
